81537ed022e5760a42847969f534c3f7220280e8
dyorgio <dyorgio@8af59bac-c343-11de-a499-5376f3b8b106>
2009-11-03 19:22:45 +0000

    implemented ConfigManager, bios cfg started.

--- trunk/jps2/src/com/jps2/util/Bios.java
+++ trunk/jps2/src/com/jps2/util/Bios.java
@@ -1,6 +1,7 @@
 package com.jps2.util;
 
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileInputStream;
 
 import com.jps2.core.memory.Memory;
 
@@ -8,14 +9,44 @@ public class Bios {
 
 	public static void load(final Memory memory) {
 		try {
-			final InputStream is = Bios.class.getClassLoader()
-					.getResourceAsStream("bios/scph39001.bin");
-			// final InputStream is =
-			// Bios.class.getClassLoader().getResourceAsStream("bios/SCPH-70012_BIOS_V12_USA_200.bin");
-			memory.writeStream(0, is);
-			is.close();
+			if (isValid(ConfigManager.getBiosFile())) {
+				final FileInputStream is = new FileInputStream(ConfigManager
+						.getBiosFile());
+				memory.writeStream(0, is);
+				is.close();
+			} else {
+				throw new RuntimeException("Inválid Bios file.");
+			}
 		} catch (final Exception e) {
 			throw new RuntimeException("Problems on load BIOS.", e);
 		}
+	}
+
+	private static final boolean isValid(final File file) {
+		try {
+			// valid bios must be at least 4mb.
+			if (file.length() < 1024 * 4096) {
+				return false;
+			}
+
+			final FileInputStream input = new FileInputStream(file);
+			int readed = -1;
+			final byte[] buffer = new byte[512];
+			final StringBuilder biosStr = new StringBuilder();
+			// read bios file as String
+			while ((readed = input.read(buffer)) != -1) {
+				biosStr.append(new String(buffer, 0, readed));
+			}
+			int index = biosStr.indexOf("RESET");
+			if (index != -1) {
+				index = biosStr.indexOf("ROMVER");
+				// TODO - Dyorgio Process bios version
+				// System.err.println(biosStr.substring(index,index+200));
+				return true;
+			}
+		} catch (final Exception e) {
+			throw new RuntimeException(e);
+		}
+		return false;
 	}
 }
---
+++ trunk/jps2/src/com/jps2/util/ConfigManager.java
@@ -0,0 +1,83 @@
+package com.jps2.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
+
+import com.jps2.gui.MainWindow;
+
+public abstract class ConfigManager {
+
+	private static final Properties props = new Properties();
+
+	private ConfigManager() {
+	}
+
+	public static final void initialize() {
+		try {
+			final File cfgFile = new File("jps2.properties");
+			if (cfgFile.exists() && cfgFile.isFile()) {
+				props.load(new FileInputStream(cfgFile));
+			} else {
+				save();
+			}
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static final void save() {
+		try {
+			props.store(new FileOutputStream("jps2.properties"),
+					"jps2 configuration file");
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static final String biosKey = "bios.file";
+
+	public static final File getBiosFile() {
+		File file = null;
+		if (props.getProperty(biosKey) == null
+				|| !(file = new File(props.getProperty(biosKey))).exists()) {
+			final JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setDialogTitle(ResourceManager
+					.getString("bios.select.title"));
+			fileChooser.setAcceptAllFileFilterUsed(false);
+			fileChooser.setFileFilter(new FileFilter() {
+
+				@Override
+				public String getDescription() {
+					return "PS2 Bios Files";
+				}
+
+				@Override
+				public boolean accept(final File f) {
+					// only show 4Mb files
+					return !f.isFile() || (f.length() > (1024 * 4096));
+				}
+			});
+
+			fileChooser.showOpenDialog(MainWindow.getInstance());
+			file = fileChooser.getSelectedFile();
+			if (file == null) {
+				JOptionPane.showMessageDialog(MainWindow.getInstance(),
+						ResourceManager.getString("bios.notfound"), "Erro",
+						JOptionPane.ERROR_MESSAGE);
+				throw new RuntimeException(ResourceManager
+						.getString("bios.notfound"));
+			} else {
+				props.setProperty(biosKey, file.getAbsolutePath());
+				save();
+			}
+
+		}
+		return file;
+	}
+}

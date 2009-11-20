0c5ead8ed97c2b2768e455cb4945bde9656dccef
dyorgio <dyorgio@8af59bac-c343-11de-a499-5376f3b8b106>
2009-11-20 19:31:25 +0000

    moved

--- trunk/jps2/src/com/jps2/plugins/zzogl/info/PathInfo.java
+++
@@ -1,8 +0,0 @@
-package com.jps2.plugins.zzogl.info;
-
-public final class PathInfo {
-	public int		mode;
-	public int		regn;
-	public long		regs;
-	public TagInfo	tag;
-}
--- trunk/jps2/src/com/jps2/plugins/zzogl/info/TagInfo.java
+++
@@ -1,7 +0,0 @@
-package com.jps2.plugins.zzogl.info;
-
-public final class TagInfo {
-	public int	nloop;
-	public int	eop;
-	public int	nreg;
-}

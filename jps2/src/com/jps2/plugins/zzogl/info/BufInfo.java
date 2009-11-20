cee6e617f4cb14f9a1c8b5616ca95e9a28a3a9fd
dyorgio <dyorgio@8af59bac-c343-11de-a499-5376f3b8b106>
2009-11-20 19:31:25 +0000

    moved

--- jps2/src/com/jps2/plugins/zzogl/info/BufInfo.java
+++
@@ -1,7 +0,0 @@
-package com.jps2.plugins.zzogl.info;
-
-public final class BufInfo {
-	public int	bp;
-	public int	bw;
-	public int	psm;
-}
--- jps2/src/com/jps2/plugins/zzogl/info/TrxPosInfo.java
+++
@@ -1,9 +0,0 @@
-package com.jps2.plugins.zzogl.info;
-
-public final class TrxPosInfo {
-	public int	sx;
-	public int	sy;
-	public int	dx;
-	public int	dy;
-	public int	dir;
-}
